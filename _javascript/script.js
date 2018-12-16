1e7fbce69932327cb75fc8e2ab718456b4130394
Sydrack Gregório <drasyck1@gmail.com>
2018-12-16 01:19:06 -0200

    modais completos mas sem fotos e js adicionado

---
+++ _javascript/script.js
@@ -0,0 +1,15 @@
+var ir = document.getElementById("ir");
+var voltar = document.getElementById("voltar");
+var modais = (document.querySelectorAll(".modal").length) ;
+
+for (let index = 1; index <= modais; index++) {
+    console.log(index);
+    
+}
+
+const irFun = function(modal){
+      
+}
+const voltarFun = function(modal){
+
+}
